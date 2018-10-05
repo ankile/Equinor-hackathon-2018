f21eabe0345b8ec99ad0ce54dfb9763b3a2c5526
tarje <tarjerw@gmail.com>
2018-10-05 23:43:04 +0200

    findShortestPathFunction

--- catkin_ws/src/equinor2018/scripts/findShortestPathFunction.py
+++ catkin_ws/src/equinor2018/scripts/findShortestPathFunction.py
@@ -17,7 +17,7 @@ def point_in_map(point,map):  # point is [x_cordinate,y_cordinate]
 
 def setup_of_lenght_map(map):
     global len_map
-    len_map
+    
     for line in map:
         new_line = []
 
