abfb94d09e80734d03f7d33986e994e35a484b3d
ankile <lars.lien@ignite.no>
2018-10-05 16:57:53 +0200

    Another bugfix

--- catkin_ws/src/equinor2018/scripts/cv.py
+++ catkin_ws/src/equinor2018/scripts/cv.py
@@ -15,58 +15,58 @@ from cv_bridge import CvBridge, CvBridgeError
 
 current_pose = None
 
-class ThreeChannelImage():
 
-	def __init__(self):
-		self.listener()
-		self.data = None
-		self.dim = (0,0)
-		self.channels = 3
-		self.bridge = CvBridge()
+class ThreeChannelImage:
 
+    def __init__(self):
+        self.listener()
+        self.data = None
+        self.dim = (0, 0)
+        self.channels = 3
+        self.bridge = CvBridge()
 
-	def callback(self, msg):
+    def callback(self, msg):
+        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
-		self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-		cv2.imshow('image',cv_image)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+        # cv2.imshow('image', self.image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
-	def listener(self):
-		 rospy.Subscriber("/drone_front_camera/image_raw", Image,  self.callback)
+    def listener(self):
+        rospy.Subscriber("/drone_front_camera/image_raw", Image, self.callback)
 
 
 def positionCallback(msg):
-	current_pose = msg
+    current_pose = msg
+
 
 def computer_vision():
-	guess = rospy.Publisher("/guess", Int8, queue_size=1)
+    guess = rospy.Publisher("/guess", Int8, queue_size=1)
 
-	rospy.init_node("cv_node", anonymous=True)
-	rospy.Subscriber("mavros/local_position/pose", PoseStamped, positionCallback)
+    rospy.init_node("cv_node", anonymous=True)
+    rospy.Subscriber("mavros/local_position/pose", PoseStamped, positionCallback)
 
-	three_channel_image = ThreeChannelImage()
-	should_guess = False
+    three_channel_image = ThreeChannelImage()
+    should_guess = False
 
-	rate = rospy.Rate(1)
+    rate = rospy.Rate(1)
 
-	while not rospy.is_shutdown():
-		"""
-		Useful variables in scope:
-			position.x
-			position.y
-			image
-		"""
-		
-		if should_guess:
-			guess.publish(1)
+    while not rospy.is_shutdown():
+        """
+        Useful variables in scope:
+            position.x
+            position.y
+            image
+        """
 
-		rate.sleep()		
+        if should_guess:
+            guess.publish(1)
+
+        rate.sleep()
 
 
-
-if __name__=='__main__':
-	try:
-		computer_vision()
-	except rospy.ROSInterruptException:
-		pass
+if __name__ == '__main__':
+    try:
+        computer_vision()
+    except rospy.ROSInterruptException:
+        pass
