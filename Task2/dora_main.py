deaab3a75fcb8d46fef176b1934eb08ee1497a15
ankile <lars.lien@ignite.no>
2018-10-07 00:40:11 +0200

    Cleanup

--- Task2/dora_main.py
+++ Task2/dora_main.py
@@ -6,6 +6,7 @@ import numpy as np
 from process_images import process_image
 from test_predictions import write_predictions_to_csv, predict_image, predict
 
+
 # images = load_unlabeled_data()
 
 def dora_main(images):
@@ -17,6 +18,7 @@ def dora_main(images):
 def predict(image):
     image = process_image(image)
     return predict_image(image)
+
 
 filename = glob.glob("labeledData/7/385.jpg")
 image = cv2.imread(filename[0])
