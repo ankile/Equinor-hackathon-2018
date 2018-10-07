3cbf79407c821e3098f7f5361ccc9248db528399
ankile <lars.lien@ignite.no>
2018-10-07 02:16:48 +0200

    Changed dora main

--- catkin_ws/src/equinor2018/scripts/dora_main.py
+++ catkin_ws/src/equinor2018/scripts/dora_main.py
@@ -2,6 +2,7 @@ import glob
 import os
 import cv2
 import numpy as np
+import random as rand
 
 from process_images import process_image
 from test_predictions import write_predictions_to_csv, predict_image, predict
@@ -17,6 +18,8 @@ def dora_main(images):
 
 def predict(image):
     image = process_image(image)
+    if image is None:
+        return rand.randint(1, 9)
     return predict_image(image)
 
 
