a692f5962e953e15c5ff579032a55e8354054147
tarje <tarjerw@gmail.com>
2018-10-07 08:45:25 +0200

    sd

--- catkin_ws/src/equinor2018/scripts/task1.py
+++ catkin_ws/src/equinor2018/scripts/task1.py
@@ -133,7 +133,7 @@ def main():
 
 
 
-        if (not drag_back_point) and speed > 0.18 and (speed**2.0)*0.107 + 0.3 > distance((pos.x, pos.y), waypoint) and distance((pos.x, pos.y), waypoint) > 1.6:
+        if (not drag_back_point) and speed > 0.2 and (speed**2.0)*0.107 + 0.3 > distance((pos.x, pos.y), waypoint) and distance((pos.x, pos.y), waypoint) > 1.6:
             path.insert(0,last_point)
 
             (x, y) = path[0]
@@ -142,7 +142,7 @@ def main():
             drag_back_point = True
 
 
-        if drag_back_point and speed < 0.08:
+        if drag_back_point and speed < 0.14:
             path = path[1:]
             (x, y) = path[0]
             drone.set_target(x, y, 0)
